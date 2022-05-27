45e79cee4ba19fd0d5a76d0a74174b8048506889
Faizanvahora120 <81639784+Faizanvahora120@users.noreply.github.com>
2022-05-26 23:35:34 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "${var.aws_region}"
-  access_key = "abcd"
-  secret_key = "abcd"
+  access_key = "AKIASDA4PIGEHRO55LUH"
+  secret_key = "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"
 }
