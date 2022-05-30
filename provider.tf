aa46b4dc8b58ec6ecbab000cdb8ff80c70cf1ae7
Faizan Vahora <vahorafaizan152@gmail.com>
2022-05-30 00:06:45 -0700

    adding s3 remote storage and locking it

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region     = "${var.aws_region}"
 }
+
+# S3 bucket to store terraform state files
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-2oi938"
+    key    = "myapp/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraformtfstate"
+  }
+}
